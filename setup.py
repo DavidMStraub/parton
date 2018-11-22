3124649efbf7c0ea75813388b3e86ea363ae6dc0
David Straub <david.straub@tum.de>
2018-11-22 13:37:04 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(name='parton',
+      version='0.1-alpha1',
+      author='David M. Straub',
+      author_email='david.straub@tum.de',
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type='text/markdown',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['numpy', 'scipy', 'setuptools', 'pyyaml'],
+      )
