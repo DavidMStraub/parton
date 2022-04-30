8df9a5a0223f6a16b6e2f05568fc16d83c58c2f6
David Straub <straub@protonmail.com>
2022-04-30 10:42:56 +0200

    Version 0.2

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
-with open('README.md', encoding='utf-8') as f:
+with open("README.md", encoding="utf-8") as f:
     LONG_DESCRIPTION = f.read()
 
-setup(name='parton',
-      version='0.1',
-      author='David M. Straub',
-      author_email='david.straub@tum.de',
-      description='Python package for parton distributions and parton luminosities',
-      long_description=LONG_DESCRIPTION,
-      long_description_content_type='text/markdown',
-      license='MIT',
-      packages=find_packages(),
-      install_requires=['numpy', 'scipy', 'setuptools', 'pyyaml', 'appdirs'],
-      )
+setup(
+    name="parton",
+    version="0.2",
+    author="David M. Straub",
+    author_email="david.straub@tum.de",
+    description="Python package for parton distributions and parton luminosities",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=["numpy", "scipy", "setuptools", "pyyaml", "appdirs"],
+)
