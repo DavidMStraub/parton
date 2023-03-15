1da2f61d2683ad79c76d4bc28ca053560e656c5e
Aleks Smolkovic <smolkovic@itp.unibe.ch>
2023-03-15 14:10:18 +0100

    Add test for pdf interpolators

--- parton/test_pdf.py
+++ parton/test_pdf.py
@@ -3,6 +3,7 @@ import tempfile
 import os
 import shutil
 from . import pdf, io
+import numpy as np
 
 
 class TestPDF(unittest.TestCase):
@@ -16,4 +17,29 @@ class TestPDF(unittest.TestCase):
                          os.path.join(dir, 'CT10', 'CT10_0001.dat'))
         meta, grids = member.load()
         pdf.PDFGrid.from_block(grids[0])
+        shutil.rmtree(dir)
+
+    def test_interpolators(self):
+        dir = tempfile.mkdtemp()
+        for pdfset in ['CT10', 'NNPDF31_nnlo_as_0118', 'NNPDF30_nnlo_as_0118']:
+            io.download_pdfset(pdfset, dir)
+            pd = pdf.PDF(pdfset, member=0, pdfdir=dir)
+
+            for grid in pd.pdfgrids:
+                logx = grid.logx
+                logQ2 = grid.logQ2
+                xfgrid = grid.xfgrid
+                m = len(logx)
+                n = len(logQ2)
+
+                for flavor in grid.flavors:
+                    grid_values = xfgrid[:, grid.flav_index(flavor)].reshape(m, n)
+                    for ix, x in enumerate(np.exp(logx)):
+                        for iQ2, Q2 in enumerate(np.exp(logQ2)):
+                            if grid_values[ix, iQ2] < 1e-10:
+                                continue
+                            else:
+                                self.assertAlmostEqual(grid.xfxQ2(flavor, x, Q2)/grid_values[ix, iQ2],
+                                        1, delta=0.01,
+                                        msg=f"Failed for {pdfset} with flavor {flavor}, x={x}, Q2={Q2} (interpolated: {grid.xfxQ2(flavor, x, Q2)}, grid: {grid_values[ix, iQ2]})")
         shutil.rmtree(dir)
